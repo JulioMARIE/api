411282b152a96d64231be0a04d4b7c6b06472ba1
JulioMARIE <julioracechoisie@gmail.com>
2019-09-24 17:25:28 +0100

    Hnadles exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,10 @@
 namespace App\Exceptions;
 
 use Exception;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +49,18 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->expectsJson()) {
+          if ($exception instanceof ModelNotFoundException) {
+            return response( [
+              'errors' => 'Product Model Not Found'
+            ], Response::HTTP_NOT_FOUND);
+          }
+          if ($exception instanceof NotFoundHttpException) {
+            return response( [
+              'errors' => 'Incorrect route'
+            ], Response::HTTP_NOT_FOUND);
+          }
+        }
         return parent::render($request, $exception);
     }
 }
